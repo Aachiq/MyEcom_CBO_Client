21504dfd02ae35b04a94d4adb2e4dc0771e0de79
Think Dev <=>
2023-09-01 22:08:05 +0100

    use Login.js not Login1.js of test

--- src/Routing.js
+++ src/Routing.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
-import Login1 from './components/Login1';
+import Login from './components/Login';
 import ProductsBo from './components/ProductsBo';
 import CategoryBo from './components/CategoryBo';
 import OrdersBo from './components/OrdersBo';
@@ -10,7 +10,7 @@ export default function Routing() {
   return (
      <div>
         <Routes>
-            <Route path="/" exact element={<Login1/>}/>
+            <Route path="/" exact element={<Login/>}/>
             <Route path="/dashboard" exact element={<Dashboard/>}/>
             <Route path="/product-bo" exact element={<ProductsBo/>}/>
             <Route path="/category-bo" exact element={<CategoryBo/>}/>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,48 +1,65 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { signInBo } from '../common/apiServices/authService'
+import { setLocalStorage } from './../common/helpers/authHelper';
+import { useNavigate } from 'react-router-dom';
 
-export default function Login() {
-  const [userLogin,setUserLogin] = useState({
-    email: '',
-    password: ''
-  })
+export default function Login1() {
+    const [userLogin,setUserLogin] = useState({
+        login: '',
+        password: ''
+    })
+    const navigate = useNavigate();
 
-  const handleChangeEmail = (event) => {
-    // add validation: phone should be 11 numbers
-    setUserLogin({...userLogin,email: event.target.value})
-  }
-  const handleChangePassword = (event) => {
-    // add validation: phone should be 11 numbers
-    setUserLogin({...userLogin,password: event.target.value})
-  }
-  const handleLoginSubmit = (event) => {
-    event.preventDefault();
-    // userLoginService(userLogin).then((res) => {
-    //   if(!res.error){
-    //    setLocalStorage(res);
-    //    navigate('/');
-    //   }
-    // });
-  }
-
+    const handleChangeLogin = (event) => {
+     setUserLogin({...userLogin,login: event.target.value})
+    }
+    const handleChangePassword = (event) => {
+     setUserLogin({...userLogin,password: event.target.value})
+    }
+    
+    const handleLoginSubmit = (event) => {
+        event.preventDefault();
+        signInBo(userLogin)
+         .then((res) => res.json())
+         .then((json) => {
+            setLocalStorage(json);
+            navigate('/dashboard')
+         })
+         .catch((err) => console.log(err))
+    }
   return (
-    <div>
-      <div className='container mt-4'>
-      <form onSubmit={handleLoginSubmit}>
-        <div className="form-group">
-          <label htmlFor="email">Email :</label>
-          <input type="text" required className='form-control' id="email" name="email" 
-                 onChange={handleChangeEmail} 
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password :</label>
-          <input type="password" required className='form-control' id="password" name="password" 
-                 onChange={handleChangePassword} 
-          />
-        </div>
-        <button className='btn btn-secondary' type="submit">Login</button>
-      </form>
+    <div className='bg-light' style={{height: '500px'}}>
+     <div style={{paddingTop: '200px', paddingLeft: '400px'}}>
+     <form onSubmit={handleLoginSubmit} style={{width: '400px'}}>
+      <div className="form-group">
+        <label htmlFor="email" style={{
+            fontFamily: 'serif',
+            fontSize: 20,
+            fontWeight: 500,
+            cursor: 'pointer'
+        }}>Login :</label>
+        <input type="text" required className='form-control' 
+               id="login" name="login" 
+               onChange={handleChangeLogin}
+        />
       </div>
+      <div className="form-group">
+        <label htmlFor="password" style={{
+            fontFamily: 'serif',
+            fontSize: 20,
+            fontWeight: 500,
+            cursor: 'pointer'
+        }}>Password :</label>
+        <input type="password" required className='form-control' 
+               id="password" name="password" 
+               onChange={handleChangePassword}
+        />
+      </div>
+      <div style={{ display:'flex', justifyContent:'center'}}>
+       <button className='btn btn-warning' type="submit">Login</button>
+      </div>
+    </form>
+     </div>
     </div>
   )
 }
